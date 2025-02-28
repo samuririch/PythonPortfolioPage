84af3df7ea7a8b1c700c44fb05b9d7ab250f037c
Sudo-Sam2017 <bigramsam75@gmail.com>
2025-02-27 21:37:08 -0500

    Added navigation menu and Contact Page.

--- Home.py
+++ Home.py
@@ -19,13 +19,25 @@ I love building things and solving puzzles - so programming is a perfect fit for
 
 st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
 
-col3, col4 = st.columns(2)
+col3, emptyCol, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](https://pythonhow.com)")
+        st.write(f"[Source Code]({row['url']})")
+
+
